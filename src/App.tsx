955ed631a11ec0be6b5b7d58f67853549263f780
Kien1509 <66570324+kienwewdwd@users.noreply.github.com>
2023-10-24 18:01:45 +0700

    updated 6PM 24/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { LogBox, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
 import React, { useEffect } from 'react';
 import MainRouter from './router/mainRouter';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 
 const App = () => {
   useEffect(() => {
